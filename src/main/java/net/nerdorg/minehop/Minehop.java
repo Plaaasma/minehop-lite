b2985d74756e2e60a1748a41fd8441805d4a6652
Plaaasma <47513553+Plaaasma@users.noreply.github.com>
2024-04-10 21:27:58 -0500

    Updated gradle.properties

--- src/main/java/net/nerdorg/minehop/Minehop.java
+++ src/main/java/net/nerdorg/minehop/Minehop.java
@@ -6,28 +6,13 @@ import net.fabricmc.api.ModInitializer;
 
 import net.nerdorg.minehop.config.MinehopConfig;
 import net.nerdorg.minehop.config.ConfigWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Minehop implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("minehop");
-    public static final String MOD_ID = "minehop";
-
 	public static HashMap<String, List<Double>> efficiencyListMap = new HashMap<>();
 	public static HashMap<String, Double> efficiencyUpdateMap = new HashMap<>();
-
-	public static List<String> adminList = List.of(
-			"lolrow",
-			"Plaaasma",
-			"_Moriz_"
-	);
 
 	@Override
 	public void onInitialize() {
