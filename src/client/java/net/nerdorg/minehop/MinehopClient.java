9c6d681ce7a144ed55e0a216536a71404e7c8136
Antropov31 <88806448+Antropov31@users.noreply.github.com>
2026-07-02 17:56:28 +0300

    port(client): MinehopClient to Mojang mappings (KeyMapping/InputConstants) for 26.2

--- src/client/java/net/nerdorg/minehop/MinehopClient.java
+++ src/client/java/net/nerdorg/minehop/MinehopClient.java
@@ -3,51 +3,51 @@ package net.nerdorg.minehop;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.nerdorg.minehop.client.SpeedometerHud;
 import net.nerdorg.minehop.config.ConfigWrapper;
 import net.nerdorg.minehop.config.MinehopConfig;
 import org.lwjgl.glfw.GLFW;
 
 public class MinehopClient implements ClientModInitializer {
-	public static SpeedometerHud speedometerHud;
+    public static SpeedometerHud speedometerHud;
 
-	public static int jump_count = 0;
-	public static boolean jumping = false;
-	public static double last_jump_speed = 0;
-	public static double start_jump_speed = 0;
-	public static double old_jump_speed = 0;
-	public static long last_jump_time = 0;
-	public static long old_jump_time = 0;
-	public static double last_efficiency;
-	public static double gauge;
-	public static boolean wasOnGround = false;
-	private static KeyBinding toggle_movement;
+    public static int jump_count = 0;
+    public static boolean jumping = false;
+    public static double last_jump_speed = 0;
+    public static double start_jump_speed = 0;
+    public static double old_jump_speed = 0;
+    public static long last_jump_time = 0;
+    public static long old_jump_time = 0;
+    public static double last_efficiency;
+    public static double gauge;
+    public static boolean wasOnGround = false;
+    private static KeyMapping toggle_movement;
+
     @Override
-	public void onInitializeClient() {
-		ConfigWrapper.loadConfig();
-		speedometerHud = new SpeedometerHud();
-		toggle_movement = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.minehop.toggle", // The translation key of the keybinding's name
-				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-				GLFW.GLFW_KEY_H, // The keycode of the key
-				"category.minehop" // The translation key of the keybinding's category.
-		));
+    public void onInitializeClient() {
+        ConfigWrapper.loadConfig();
+        speedometerHud = new SpeedometerHud();
+        toggle_movement = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.minehop.toggle", // The translation key of the keybinding's name
+                InputConstants.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_H, // The keycode of the key
+                "category.minehop" // The translation key of the keybinding's category.
+        ));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (client.player != null) {
-				if (client.options.jumpKey.isPressed()) {
-					jumping = true;
-				}
-				else {
-					jumping = false;
-				}
-				while (toggle_movement.wasPressed()) {
-					MinehopConfig.enabled = !MinehopConfig.enabled;
-				}
-			}
-		});
-	}
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null) {
+                if (client.options.keyJump.isDown()) {
+                    jumping = true;
+                }
+                else {
+                    jumping = false;
+                }
+                while (toggle_movement.consumeClick()) {
+                    MinehopConfig.enabled = !MinehopConfig.enabled;
+                }
+            }
+        });
+    }
 }
